126beb3d32584d890496ef173bcd05d845fa2c5a
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-11 23:14:49 +0500

    authenticated customer

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,3 +29,21 @@ export const register = async(req, res)=>{
         res.status(500).json({error: error.message});
     }
 }
+
+/* LOGGING IN */
+export const login = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+        const customer = await Customer.findOne({email: email});
+        if(!customer) return res.status(400).json({msg: "Customer does not exist"});
+        
+        const isMatch = await bcrypt.compare(password, customer.password);
+        if(!isMatch) return res.status(400).json({msg: "Invalid credentials. "});
+
+        const token = jwt.sign({id: customer._id}, process.env.JWT_SECRET);
+        delete customer.password;
+        res.status(200).json({token, customer});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
