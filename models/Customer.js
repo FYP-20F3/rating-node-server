5ef5910b1ae9e34773aac1e932131a7f25e289a4
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-11 20:01:10 +0500

    imported pkgs

---
+++ models/Customer.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const CustomerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    picturePath: {
+        type: String,
+        default: []
+    }
+}, {timestamps: true})
+
+const Customer = mongoose.model("Customer", CustomerSchema);
+
