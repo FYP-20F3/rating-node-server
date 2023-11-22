30bf20854d326421ac8ee0974b68720351f219db
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-22 22:23:17 +0500

    tweeked reviews model

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,24 +4,24 @@ import Business from "../models/Business.js";
 /* CREATE */
 export const createReview = async (req, res)=>{
     try {
-        let { businessId, reviewSource, reviewRating, reviewType, reviewTitle, reviewDescription, dateOfExperience, reviewClassificationId } = req.body;
+        let { businessId, reviewSource, reviewRating, reviewType, reviewTitle, reviewDescription,Sentiment, reviewClassificationId } = req.body;
 
         const business = await Business.findOne({businessName: businessId});
 
         const newReview = new Review({
             customerId: req.customer.id, 
-            businessId: business._id, 
+            businessId: business.id, 
             reviewSource, 
             reviewRating, 
             reviewType, 
             reviewTitle, 
-            reviewDescription, 
-            dateOfExperience, 
+            reviewDescription,
+            Sentiment, 
             reviewClassificationId
         });
         await newReview.save();
         const reviews = await Review.find();
-        res.status(201).json(business);
+        res.status(201).json(reviews);
 
     } catch (error) {
         res.status(409).json({msg: error.message});
