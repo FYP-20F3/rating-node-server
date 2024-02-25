cfebddbb190bd983075e9a8302f8f540d49b418f
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-25 13:50:21 +0500

    removed category route

--- routes/businesses.js
+++ routes/businesses.js
@@ -9,11 +9,6 @@ const router = express.Router();
 
 /* READ */
 router.get("/", getAllBusinesses);
-<<<<<<< HEAD
-router.get("/category",verifyToken, getBusinessCategory);
 router.get("/:categoryName",verifyToken, getBusinessByCategory);
-=======
-router.get("/:categoryName", getBusinessesByCategory);
->>>>>>> 10cb7bf80a63b890f78445926279190f6c6d8c7f
 
 export default router;
