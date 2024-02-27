de7f0345bd8c40d9b1ca36638ab96f8d5e627419
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-28 02:08:49 +0500

    got businesses with queryparameter

--- controllers/businesses.js
+++ controllers/businesses.js
@@ -3,15 +3,39 @@ import {countBusinessReviews} from "../controllers/reviews.js";
 /* READ */
 export const getAllBusinesses = async (req, res) => {
   try {
-    const allBusinesses = await Business.find();
+    // Construct the query object with dynamic filters
+    let queryObj = {};
+
+    if (req.query.category) {
+      queryObj.businessCategory = req.query.category;
+    }
+
+    if (req.query.location) {
+      queryObj.location = req.query.location;
+    }
+
+    if (req.query.rating) {
+      // Assuming rating is a numeric value and you want businesses with a rating greater or equal
+      queryObj.overallRating = { $gte: Number(req.query.rating) };
+    }
+
+    // Use the query object in the find method
+    const allBusinesses = await Business.find(queryObj,
+      { password: 0 } // Exclude the 'password' field
+    );
+
     const businessesWithReviewCount = await Promise.all(
-    allBusinesses.map(async (business) => {
+      allBusinesses.map(async (business) => {
         // Clone the business object to avoid mutating the original document
         const businessObject = business.toObject();
         businessObject.reviewCount = await countBusinessReviews(business.id);
         return businessObject;
-    })
+      })
     );
+
+    // Sort businesses by reviewCount in descending order
+    businessesWithReviewCount.sort((a, b) => b.reviewCount - a.reviewCount);
+
     res.status(200).json(businessesWithReviewCount);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -23,7 +47,7 @@ export const getBusinessesByName = async (req, res) => {
     const { businessName } = req.params;
     const business = await Business.findOne(
       { businessName: businessName },
-      { password: 0 } // Exclude the 'password' field
+      { password: 0 },// Exclude the 'password' field
     );
 
     const reviewCount = await countBusinessReviews(business.id);
@@ -41,8 +65,6 @@ export const getBusinessesByLocation = async (req, res) => {
       { location: location },
       { password: 0 } // Exclude the 'password' field
     );
-
-    // const reviewCount = await countBusinessReviews(business.id);
     
     res.status(200).json({ business });
   } catch (error) {
