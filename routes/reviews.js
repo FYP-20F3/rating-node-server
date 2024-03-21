6d44b86ac46c684f8e085d0ce144e96195371b38
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-03-22 03:50:19 +0500

    added reviewReply model

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {getCustomerReviews, getBusinessReviews, createReview } from "../controllers/reviews.js";
+import {postReview, getReviewReply } from "../controllers/reviewReply.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -11,5 +12,7 @@ router.post("/create",verifyToken, createReview);
 router.get("/customer/:customerId",verifyToken, getCustomerReviews);
 router.get("/business/:businessId",verifyToken, getBusinessReviews);
 
+router.post('/replies', postReview);
+router.get('/:reviewId/replies', getReviewReply);
 
 export default router;
