77182f6c59ba183df2b99f546d4428f939b3395f
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-03-23 17:56:48 +0500

    added reviewReply model

--- models/Review.js
+++ models/Review.js
@@ -41,7 +41,7 @@ const reviewSchema = mongoose.Schema(
     dateOfExperience: {
       type: Date,
       default: Date.now,
-    }
+    },
     Sentiment: {
       type: String,
       default: "Neutral",
