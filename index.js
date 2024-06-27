1a84aba88fbf65b6f68bc3c1dfbb95782802ccb4
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-06-28 01:59:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use("/message", messageRoutes);
 app.use("/complaint", complaintRoutes);
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://rating-frontend.netlify.app/"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
